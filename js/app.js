6d87aa4ec13c451dd54afd5cb74c275f015baaa3
Brian Ginsburg <brian@brianginsburg.com>
2016-07-02 11:16:18 -0700

    Add a second note
    Change logic for starting notes.
    Rename functions
    Update html

--- js/app.js
+++ js/app.js
@@ -3,27 +3,37 @@ $(document).foundation()
 context = new AudioContext()
 
 // declare globals
-var note,
+var noteOne, noteTwo,
     volume = context.createGain()
 
 // initialize volume
 volume.gain.value = 0.1
 
-function initNote(freq) {
-    note = context.createOscillator()
+function initNote(note, freq) {
     note.type = 'sawtooth'
     note.frequency.value = freq
     note.connect(volume)
     volume.connect(context.destination)
 }
 
-function startSaw() {
-    initNote(440)
-    note.start(0)          
+function startNoteOne() {
+    noteOne = context.createOscillator()
+    initNote(noteOne,440)
+    noteOne.start(0)          
 }
 
-function stopSaw() {
-    note.stop()
+function stopNoteOne() {
+    noteOne.stop()
+}
+
+function startNoteTwo() {
+    noteTwo = context.createOscillator()
+    initNote(noteTwo,880)
+    noteTwo.start(0)          
+}
+
+function stopNoteTwo() {
+    noteTwo.stop()
 }
 
 // Gain input event changes level from 0 to 1 
