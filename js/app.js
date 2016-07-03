abf13b4a2ce94f5030a4ebd7aafdb4b50106c9bc
Brian Ginsburg <brian@brianginsburg.com>
2016-07-02 20:03:18 -0700

    Wire up remainder of keyboard and click events

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ var voiceOne, voiceTwo,
 // initialize volume
 volume.gain.value = 0.1
 
+// initialize pitch values
 $(document).ready(function(){
     $('#voice-one-pitch').attr("value","440")
     $('#voice-two-pitch').attr("value","493.88")
@@ -21,6 +22,13 @@ $(document).ready(function(){
     $('#voice-eight-pitch').attr("value","880")
 });
 
+// Gain input event changes level from 0 to 1 
+$('#gain').on('input',function(e){
+    gain = $("#gain").val()
+    volume.gain.value = gain
+});
+
+// initialize a new note
 function initVoice(voice, freq) {
     voice.type = 'triangle'
     console.log(freq)
@@ -29,6 +37,8 @@ function initVoice(voice, freq) {
     volume.connect(context.destination)
 }
 
+
+// start and stop voices
 function startVoiceOne() {
     var freq = $('#voice-one-pitch').val()
     voiceOne = context.createOscillator()
@@ -51,11 +61,73 @@ function stopVoiceTwo() {
     voiceTwo.stop()
 }
 
-// Gain input event changes level from 0 to 1 
-$('#gain').on('input',function(e){
-    gain = $("#gain").val()
-    volume.gain.value = gain
-});
+function startVoiceThree() {
+    var freq = $('#voice-three-pitch').val()
+    voiceThree = context.createOscillator()
+    initVoice(voiceThree,freq)
+    voiceThree.start(0)          
+}
+
+function stopVoiceThree() {
+    voiceThree.stop()
+}
+
+
+function startVoiceFour() {
+    var freq = $('#voice-four-pitch').val()
+    voiceFour = context.createOscillator()
+    initVoice(voiceFour,freq)
+    voiceFour.start(0)          
+}
+
+function stopVoiceFour() {
+    voiceFour.stop()
+}
+
+function startVoiceFive() {
+    var freq = $('#voice-five-pitch').val()
+    voiceFive = context.createOscillator()
+    initVoice(voiceFive,freq)
+    voiceFive.start(0)          
+}
+
+function stopVoiceFive() {
+    voiceFive.stop()
+}
+
+function startVoiceSix() {
+    var freq = $('#voice-six-pitch').val()
+    voiceSix = context.createOscillator()
+    initVoice(voiceSix,freq)
+    voiceSix.start(0)          
+}
+
+function stopVoiceSix() {
+    voiceSix.stop()
+}
+
+function startVoiceSeven() {
+    var freq = $('#voice-seven-pitch').val()
+    voiceSeven = context.createOscillator()
+    initVoice(voiceSeven,freq)
+    voiceSeven.start(0)          
+}
+
+function stopVoiceSeven() {
+    voiceSeven.stop()
+}
+
+function startVoiceEight() {
+    var freq = $('#voice-eight-pitch').val()
+    voiceEight = context.createOscillator()
+    initVoice(voiceEight,freq)
+    voiceEight.start(0)          
+}
+
+function stopVoiceEight() {
+    voiceEight.stop()
+}
+
 
 // ---------- click events ------------
 $('#voice-one').mousedown(function() {
@@ -74,6 +146,53 @@ $('#voice-two').mouseup(function() {
     stopVoiceTwo();
 });
 
+$('#voice-three').mousedown(function() {
+    startVoiceThree();
+});
+
+$('#voice-three').mouseup(function() {
+    stopVoiceThree();
+});
+
+$('#voice-four').mousedown(function() {
+    startVoiceFour();
+});
+
+$('#voice-four').mouseup(function() {
+    stopVoiceFour();
+});
+
+$('#voice-five').mousedown(function() {
+    startVoiceFive();
+});
+
+$('#voice-five').mouseup(function() {
+    stopVoiceFive();
+});
+
+$('#voice-six').mousedown(function() {
+    startVoiceSix();
+});
+
+$('#voice-six').mouseup(function() {
+    stopVoiceSix();
+});
+
+$('#voice-seven').mousedown(function() {
+    startVoiceSeven();
+});
+
+$('#voice-seven').mouseup(function() {
+    stopVoiceSeven();
+});
+
+$('#voice-eight').mousedown(function() {
+    startVoiceEight();
+});
+
+$('#voice-eight').mouseup(function() {
+    stopVoiceEight();
+});
 // ---------- keypress events ------------
 // keydown starts notes, keyup stops note
 // voices keeps track of which keys are currently held
@@ -93,16 +212,22 @@ $(document).keydown(function(e) {
             startVoiceTwo()
             break
         case 68:
+            startVoiceThree()
             break
         case 70:
+            startVoiceFour();
             break
         case 74:
+            startVoiceFive();
             break
         case 75:
+            startVoiceSix();
             break
         case 76:
+            startVoiceSeven();
             break
         case 59:
+            startVoiceEight();
             break
     }
     voices[e.which] = true
@@ -117,16 +242,22 @@ $(document).keyup(function(e) {
             stopVoiceTwo()
             break
         case 68:
+            stopVoiceThree()
             break
         case 70:
+            stopVoiceFour()
             break
         case 74:
+            stopVoiceFive();
             break
         case 75:
+            stopVoiceSix();
             break
         case 76:
+            stopVoiceSeven();
             break
         case 59:
+            stopVoiceEight();
             break
     }
     voices[e.which] = false
