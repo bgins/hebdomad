e0600231eec242e4904fcd76d60300958f731812
Brian Ginsburg <brian@brianginsburg.com>
2016-07-02 19:43:28 -0700

    Add value intializer. Fiddle with sizing.
    Initial values set to 12-EDO major scale on A
    Made keyboard larger to give space for decimal values

--- js/app.js
+++ js/app.js
@@ -10,16 +10,29 @@ var voiceOne, voiceTwo,
 // initialize volume
 volume.gain.value = 0.1
 
+$(document).ready(function(){
+    $('#voice-one-pitch').attr("value","440")
+    $('#voice-two-pitch').attr("value","493.88")
+    $('#voice-three-pitch').attr("value","523.33")
+    $('#voice-four-pitch').attr("value","587.33")
+    $('#voice-five-pitch').attr("value","659.26")
+    $('#voice-six-pitch').attr("value","698.46")
+    $('#voice-seven-pitch').attr("value","783.99")
+    $('#voice-eight-pitch').attr("value","880")
+});
+
 function initVoice(voice, freq) {
-    voice.type = 'sawtooth'
+    voice.type = 'triangle'
+    console.log(freq)
     voice.frequency.value = freq
     voice.connect(volume)
     volume.connect(context.destination)
 }
 
 function startVoiceOne() {
+    var freq = $('#voice-one-pitch').val()
     voiceOne = context.createOscillator()
-    initVoice(voiceOne,440)
+    initVoice(voiceOne,freq)
     voiceOne.start(0)          
 }
 
@@ -28,8 +41,9 @@ function stopVoiceOne() {
 }
 
 function startVoiceTwo() {
+    var freq = $('#voice-two-pitch').val()
     voiceTwo = context.createOscillator()
-    initVoice(voiceTwo,880)
+    initVoice(voiceTwo,freq)
     voiceTwo.start(0)          
 }
 
