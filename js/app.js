61a01c6f8624be820cefdddcee87332dfdb753d1
Brian Ginsburg <brian@brianginsburg.com>
2016-07-06 20:12:11 -0700

    Revert "Start experiment to add AR envelope"
    This reverts commit 36e08d2690054fd73514952a921b12bd59b3c56f.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ context = new AudioContext()
 // declare globals
 var voices = []
     heldKeys = [],
-    voiceAmps = []
+    amp = context.createGain()
 
 
 $(document).ready(function() {
@@ -22,14 +22,11 @@ function initTuning() {
     $('#voice-six-pitch').attr("value","684.44")
     $('#voice-seven-pitch').attr("value","770")
     $('#voice-eight-pitch').attr("value","880")
-    $('#gain').attr("value","5")
+    $('#gain').attr("value","1")
 }
 
-// create and initialize master gain
-amp = context.createGain()
-amp.gain.value = 0.5
-amp.connect(context.destination)
-
+// initialize gain
+amp.gain.value = 0.1
 
 // ---------- start and stop voices------------
 function startVoice(n,freq) {
@@ -37,21 +34,11 @@ function startVoice(n,freq) {
     voices[n].type = 'triangle'
     console.log(freq)
     voices[n].frequency.value = freq
-
-    voiceAmps[n] = context.createGain()
-    voiceAmps[n].gain.value = 0
-    voiceAmps[n].gain.setTargetAtTime(0.1, context.currentTime, 0.5)
-
-    voices[n].connect(voiceAmps[n])
-    voiceAmps[n].connect(amp)
+    voices[n].connect(amp)
+    amp.connect(context.destination)
     voices[n].start(0)          
-    // voiceAmps[n].gain.setTargetAtTime(0, context.currentTime + 2, 0.1)
-    voiceAmps[n].gain.linearRampToValueAtTime(0.01, context.currentTime + 1)
 }
 
 function stopVoice(n) {
-    // voiceAmps[n].gain.linearRampToValueAtTime(0.01, context.currentTime + 1)
-    // voiceAmps[n].gain.setTargetAtTime(0, context.currentTime, 0.2)
-    voices[n].stop(context.currentTime + 1)
-    voiceAmps[n].disconnect()
+    voices[n].stop()
 }
